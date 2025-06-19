7761c4a85c2c5d043b0cdbf92d626aa2b85e91f4
TahaA-7 <152402600+TahaA-7@users.noreply.github.com>
2025-06-19 14:27:45 +0200

    a

--- DataModels/UserModel.py
+++ DataModels/UserModel.py
@@ -1,9 +1,10 @@
 from datetime import datetime 
+from presentation_layer.utils.Roles import Roles
 
 # ADDED `is_active` FLAG AND GAVE IT A DEFAULT VALUE ALONG WITH `role` TO ALLOW SELF-INITIALISATION BEFORE ADMIN-VALIDATION
 
 class User:
-    def __init__(self, username: str, hashed_password: str, first_name: str, last_name: str, role=1, is_active=False):
+    def __init__(self, username: str, hashed_password: str, first_name: str, last_name: str, role=Roles.SERVICE_ENGINEER, is_active=False):
         self.username = username.lower()
         self.hashed_password = hashed_password  # Hashed, not plain text!
         self.role = role  # "super_admin=3", "system_admin=2", "service_engineer=1"
